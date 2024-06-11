7004674ec01e5951c0fee4b0d3e56370cf2a4a09
vicentemartv <vicente.martinezvs@gmail.com>
2024-06-11 17:06:39 -0600

    Hola jaja

--- alarm-parser.py
+++ alarm-parser.py
@@ -8,7 +8,7 @@ from utils import queryMachineRefs, getSources, identifySources, getsourceId, ge
 
 load_dotenv()
 api_key = os.getenv('deacero_api_key')
-st.write("Hola Perros")
+st.write("Hola Perrossss")
 
 if api_key:
     st.write("API Key loaded successfully!")
